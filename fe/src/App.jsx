74578f55c5571a83d9269a89c20b39bdb8809754
marcoismakalew <marcoismakalew@gmail.com>
2026-02-14 20:02:21 +0800

    set up routes

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,26 +1,33 @@
-import {useState} from "react";
-import {Route, Routes} from "react-router";
+import { useState } from "react";
+import { Route, Routes } from "react-router";
 /* Pages */
 import LandingPage from "./pages/LandingPage";
 import GuruBeranda from "./pages/Guru/GuruBeranda.jsx";
 import GuruPresensi from "./pages/Guru/GuruPresensi.jsx";
+import LoginPage from "./pages/Login.jsx";
 
 /* Layouts */
 import UserLayout from "./layouts/UserLayout.jsx";
 
 function App() {
-
-    return (<>
-        <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route>
-                <Route path="/user" element={<UserLayout/>}>
-                    <Route path={'beranda'} element={<GuruBeranda/>}/>
-                    <Route path={'presensi'} element={<GuruPresensi/>}/>
-                </Route>
-            </Route>
-        </Routes>
-    </>);
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/user" element={<UserLayout />}>
+					<Route path={"beranda"} element={<GuruBeranda />} />
+					<Route path={"presensi"} element={<GuruPresensi />} />
+				</Route>
+				<Route path="/admin" element={<UserLayout />}>
+					<Route path={"beranda"} element={<AdminBeranda />} />
+					<Route path={"kelas"} element={<AdminKelas />} />
+					<Route path={"siswa"} element={<AdminSiswa />} />
+					<Route path={"mapel"} element={<AdminMapel />} />
+				</Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
