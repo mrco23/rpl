9c6e7f4190f95761de15775373800762de7f7225
marcoismakalew <marcoismakalew@gmail.com>
2026-04-17 09:21:26 +0800

    refactor(api): ekstrakurikuler

--- be/src/routes/ekstrakurikulerRoutes.js
+++ be/src/routes/ekstrakurikulerRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import {
-  getAll,
-  getPublic,
-  getDetailPublic,
-  create,
-  updateData,
-  updateImage,
-  remove,
+	getAll,
+	getPublic,
+	getDetailPublic,
+	create,
+	updateData,
+	updateImage,
+	remove,
 } from "../controllers/EkstrakurikulerController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import upload from "../middleware/uploadMiddleware.js";
@@ -14,48 +14,27 @@ import upload from "../middleware/uploadMiddleware.js";
 const ekstrakurikulerRoutes = express.Router();
 
 // PUBLIC
-ekstrakurikulerRoutes.get("/public", getPublic);
+ekstrakurikulerRoutes.get("/", getPublic);
 ekstrakurikulerRoutes.get("/public/:id", getDetailPublic);
-
-// PRIVATE
-ekstrakurikulerRoutes.get("", verifyToken, getAll);
 
 /* 
   POST /: Membuat data baru beserta upload gambar awal.
 */
-ekstrakurikulerRoutes.post(
-  "",
-  verifyToken,
-  upload.single("gambar"),
-  create
-);
+ekstrakurikulerRoutes.post("", verifyToken, upload.single("gambar"), create);
 
 /* 
   PUT /:id : Update data non-file.
 */
-ekstrakurikulerRoutes.put(
-  "/:id",
-  verifyToken,
-  updateData
-);
+ekstrakurikulerRoutes.put("/:id", verifyToken, updateData);
 
 /* 
   PATCH /:id/image : Khusus update gambar (gambar lama dihapus otomatis di logic).
 */
-ekstrakurikulerRoutes.patch(
-  "/:id/image",
-  verifyToken,
-  upload.single("gambar"),
-  updateImage
-);
+ekstrakurikulerRoutes.patch("/:id/image", verifyToken, upload.single("gambar"), updateImage);
 
 /* 
   DELETE /:id : Hapus data berserta gambar fisiknya.
 */
-ekstrakurikulerRoutes.delete(
-  "/:id",
-  verifyToken,
-  remove
-);
+ekstrakurikulerRoutes.delete("/:id", verifyToken, remove);
 
 export default ekstrakurikulerRoutes;
